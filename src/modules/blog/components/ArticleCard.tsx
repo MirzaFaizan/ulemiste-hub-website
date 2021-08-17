09a80e2a66a5f6bdf8545dced15132bff1924fb1
Faizan Ejaz <faizan.ejaz@vntrs.com>
2021-08-17 17:17:32 +0300

    Chore(article card)

--- src/modules/blog/components/ArticleCard.tsx
+++ src/modules/blog/components/ArticleCard.tsx
@@ -1,8 +1,6 @@
 import { IArticle } from '@/modules/blog/types';
-import React, { ReactElement } from 'react';
-import { FC } from 'react';
-import Image from 'next/image';
 import { useRouter } from 'next/router';
+import React, { FC, ReactElement } from 'react';
 
 interface IProps {
     article: IArticle;
@@ -14,19 +12,32 @@ const ArticleCard: FC<IProps> = ({ article }): ReactElement => {
         <div
             className="lg:pt-12 pt-6 w-full md:w-4/12 px-4 text-center min-w-full"
             onClick={() => router.push(`/blog/${article.id}`)}>
-            <div className="grid grid-cols-10 grid-flow-col min-w-0 break-words bg-rose-200 w-full mb-8 shadow-lg  p-4">
-                <div className="mb-2 relative  md:m-4 md:mt-0 col-span-3">
-                    <Image
-                        alt="Article Image"
-                        src={article.featuredImage ?? '/assets/logo.png'}
-                        layout="intrinsic"
-                        height="50"
-                        width="80"
+            <div className=" rounded overflow-hidden shadow-lg">
+                <img
+                    className="w-full"
+                    src={article.featuredImage}
+                    alt="article feature Image"
+                />
+                <div className="px-6 py-4">
+                    <div className="font-bold text-xl mb-4">
+                        {article.title}
+                    </div>
+                    <div
+                        className="text-gray-700 text-base"
+                        dangerouslySetInnerHTML={{ __html: article.excerpt }}
                     />
                 </div>
-                <div className="px-4 py-5 col-span-7">
-                    <h6 className="text-xl font-semibold ">{article.title}</h6>
-                </div>
+                {/* <div className="px-6 pt-4 pb-2">
+                    <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+                        #photography
+                    </span>
+                    <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+                        #travel
+                    </span>
+                    <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+                        #winter
+                    </span>
+                </div> */}
             </div>
         </div>
     );
